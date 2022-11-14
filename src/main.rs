7190d37b35a0418912e6033b7dd3d659d780f4f2
jakubtyrcha <jakub.tyrcha@gmail.com>
2022-11-14 00:21:38 +0000

    split audio logic

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::iter;
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
 
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use audio::{SampleRequestOptions, stream_setup_for};
+use cpal::traits::{StreamTrait};
 
 use egui::Context;
 use ::egui::FontDefinitions;
@@ -14,6 +15,8 @@ use winit::event::Event::*;
 use winit::event_loop::ControlFlow;
 const INITIAL_WIDTH: u32 = 1920;
 const INITIAL_HEIGHT: u32 = 1080;
+
+mod audio;
 
 struct MyApp {
     stream: cpal::Stream,
@@ -29,13 +32,12 @@ impl Default for MyApp {
     fn default() -> Self {
         let state = Arc::new(Mutex::new(SharedState{ freq: 1000.0, volume: 1.0 }));
         let state_clone = Arc::clone(&state);
-        let callback = move |o: &mut SampleRequestOptions| {
-            o.tick();
-            let state = state_clone.lock().unwrap();
-            o.tone(state.freq) * state.volume
-        };
         let result = MyApp {
-            stream: stream_setup_for(callback).unwrap(),
+            stream: stream_setup_for(move |o: &mut SampleRequestOptions| {
+                o.tick();
+                let state = state_clone.lock().unwrap();
+                o.tone(state.freq) * state.volume
+            }).unwrap(),
             state: state,
         };
         result.stream.play().unwrap();
@@ -52,92 +54,6 @@ impl MyApp {
                 ui.add(egui::Slider::new(&mut state.as_mut().unwrap().freq, 10.0..=1000.0).text("Frequency"));
                 ui.add(egui::Slider::new(&mut state.as_mut().unwrap().volume, 0.0..=2.0).text("Volume"));     
             });
-    }
-}
-
-pub struct SampleRequestOptions {
-    pub sample_rate: f32,
-    pub sample_clock: f32,
-    pub nchannels: usize,
-}
-
-impl SampleRequestOptions {
-    fn tone(&self, freq: f32) -> f32 {
-        (self.sample_clock * freq * 2.0 * std::f32::consts::PI / self.sample_rate).sin()
-    }
-    fn tick(&mut self) {
-        self.sample_clock = (self.sample_clock + 1.0) % self.sample_rate;
-    }
-}
-
-pub fn stream_setup_for<F>(on_sample: F) -> Result<cpal::Stream, anyhow::Error>
-where
-    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static + Clone,
-{
-    let (_host, device, config) = host_device_setup()?;
-
-    match config.sample_format() {
-        cpal::SampleFormat::F32 => stream_make::<f32, _>(&device, &config.into(), on_sample.clone()),
-        cpal::SampleFormat::I16 => stream_make::<i16, _>(&device, &config.into(), on_sample.clone()),
-        cpal::SampleFormat::U16 => stream_make::<u16, _>(&device, &config.into(), on_sample.clone()),
-    }
-}
-
-pub fn host_device_setup(
-) -> Result<(cpal::Host, cpal::Device, cpal::SupportedStreamConfig), anyhow::Error> {
-    let host = cpal::default_host();
-
-    let device = host
-        .default_output_device()
-        .ok_or_else(|| anyhow::Error::msg("Default output device is not available"))?;
-    println!("Output device : {}", device.name()?);
-
-    let config = device.default_output_config()?;
-    println!("Default output config : {:?}", config);
-
-    Ok((host, device, config))
-}
-
-pub fn stream_make<T, F>(
-    device: &cpal::Device,
-    config: &cpal::StreamConfig,
-    on_sample: F,
-) -> Result<cpal::Stream, anyhow::Error>
-where
-    T: cpal::Sample,
-    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static + Clone,
-{
-    let sample_rate = config.sample_rate.0 as f32;
-    let sample_clock = 0f32;
-    let nchannels = config.channels as usize;
-    let mut request = SampleRequestOptions {
-        sample_rate,
-        sample_clock,
-        nchannels,
-    };
-    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
-
-    let stream = device.build_output_stream(
-        config,
-        move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
-            on_window(output, &mut request, on_sample.clone())
-        },
-        err_fn,
-    )?;
-
-    Ok(stream)
-}
-
-fn on_window<T, F>(output: &mut [T], request: &mut SampleRequestOptions, mut on_sample: F)
-where
-    T: cpal::Sample,
-    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static,
-{
-    for frame in output.chunks_mut(request.nchannels) {
-        let value: T = cpal::Sample::from::<f32>(&on_sample(request));
-        for sample in frame.iter_mut() {
-            *sample = value;
-        }
     }
 }
 
